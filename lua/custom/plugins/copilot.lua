0a3b2517a4b17903a629b21ffc8c211b551a9b04
alexandro scalese <alexandro.scalese@gmail.com>
2026-04-10 09:32:43 +0200

    add copilot suggestions, lazygit and nightfox colorscheme

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
+    end,
+  },
+  {
+    'fang2hou/blink-copilot',
+    dependencies = { 'zbirenbaum/copilot.lua' },
+  },
+}
---
+++ lua/custom/plugins/nightfox.lua
@@ -0,0 +1,15 @@
+return {
+  'EdenEast/nightfox.nvim',
+  priority = 1000,
+  config = function()
+    require('nightfox').setup {
+      options = {
+        -- Compiled file's destination location
+        colorblind = {
+          enable = true,
+        },
+        styles = {},
+      },
+    }
+  end,
+}
